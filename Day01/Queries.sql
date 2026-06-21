96113aa6534b12717840378073915d80b811de58
Pooja Khape <poojakhape08@gmail.com>
2026-06-21 12:24:31 +0530

    Update Queries.sql

--- Day01/Queries.sql
+++ Day01/Queries.sql
@@ -1,7 +1,10 @@
+-- Display all columns from students table
 SELECT * FROM students;
 
+-- Display only student names
 SELECT name
 FROM students;
 
+-- Display name and city
 SELECT name, city
 FROM students;
